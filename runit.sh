cf4085b6e7d4b3ca35a5319a36c07b5fbfea0f75
Isaac Johnson <johnsi10@medtronic.com>
2021-07-08 07:48:33 -0500

    updates (some issues with Node 14 and the node package, switched to 10 which i use locally)

--- runit.sh
+++ runit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
 docker build -f Dockerfile -t nodesqswatcher .
-docker tag nodesqswatcher:latest harbor.freshbrewed.science/freshbrewedprivate/nodesqswatcher:1.12
-docker push harbor.freshbrewed.science/freshbrewedprivate/nodesqswatcher:1.12
+docker tag nodesqswatcher:latest harbor.freshbrewed.science/freshbrewedprivate/nodesqswatcher:1.13
+docker push harbor.freshbrewed.science/freshbrewedprivate/nodesqswatcher:1.13
 
-sed -i 's/image:\(.*\):\([^:]*\)/image:\1:1.12/' sqswatcher.dep.yaml
+sed -i 's/image:\(.*\):\([^:]*\)/image:\1:1.13/' sqswatcher.dep.yaml
 kubectl apply -f sqswatcher.dep.yaml
