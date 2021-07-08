88df64a0c128656be7c1294b3bf4384d098771e5
Isaac Johnson <johnsi10@medtronic.com>
2021-07-08 11:49:12 -0500

    cleanup debug

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const bodyParser = require('body-parser');
 const { exec } = require("child_process");
 var MIME = require('@ronomon/mime');
 const fs = require('fs');
+const https = require('https')
 const app = express();
 app.use(bodyParser.json())
 
 const port = 8080
+
+//file system based MIME decoding
 app.post('/mysqsforsnsOLD', (req, res) => {
     console.log(req.body);
 
@@ -37,7 +40,7 @@ app.post('/mysqsforsnsOLD', (req, res) => {
     res.status(200).send()
 })
 
-
+// using @ronomon/mime
 app.post('/mysqsforsns', (req, res) => {
 
     var mymsg = JSON.parse(req.body.Message);
@@ -47,10 +50,14 @@ app.post('/mysqsforsns', (req, res) => {
 
     var buf = Buffer.from(mymsg.content, 'utf8')
     var mime = new MIME.Message(buf);
+    /*
     console.log(mime.body.toString());
     console.log(mime.to);
+    console.log(mime.from[0].name.toString());
+    console.log(mime.from[0].email.toString());
     console.log(mime.parts[0].body.toString());
     console.log(mime.parts[0].contentType.value);
+    */
     const re = /REQ(\d+)/i;
     mime.parts.forEach(element => {
         console.log("element...");
@@ -65,36 +72,50 @@ app.post('/mysqsforsns', (req, res) => {
             console.log("MATCHED")
             var found = str.match(re);
             console.log(found[1]);
+
+            // due to all the complicated messages we may get, just base64 it and send that as the payload
+            let strbuff = Buffer.from(str, 'utf8')
+            let b64data = strbuff.toString('base64')
+
+            const data = JSON.stringify({
+                fromname: mime.from[0].name.toString(),
+                fromemail: mime.from[0].email.toString(),
+                workitem: found[1],
+                comment: b64data
+              });
+
+              console.log("str--------")
+              console.log(str)
+              console.log("b64data--------")
+              console.log(b64data)
+
+              const options = {
+                hostname: 'dev.azure.com',
+                port: 443,
+                path: '/princessking/_apis/public/distributedtask/webhooks/emailbasedUpdates?api-version=6.0-preview',
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Content-Length': data.length
+                }
+              }
+
+              const req = https.request(options, res => {
+                console.log(`statusCode: ${res.statusCode}`)
+              
+                res.on('data', d => {
+                  process.stdout.write(d)
+                })
+              })
+              
+              req.on('error', error => {
+                console.error(error)
+              })
+              
+              req.write(data)
+              req.end()
         }
-
-//console.log(found);
     });
-    console.log(mime.from);
-
-    fs.writeFile('mimefile', mymsg.content, function (err) {
-        if (err) return console.log(err);
-        console.log('mimefile created');
-
-        exec("touch part0 && rm -f part* &&  munpack -f -t mimefile", (error, stdout, stderr) => {
-            if (error) {
-                console.log(`create error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`create stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-            
-            const data = fs.readFileSync('part1', {encoding:'utf8', flag:'r'});
-            const data2 = fs.readFileSync('part2', {encoding:'utf8', flag:'r'});
-
-            console.log(`data: ${data}`);
-            console.log(`data2: ${data2}`);
-        });
-        //const execSync = require('child_process').execSync;
-        //code = execSync('munpack -f -t mimemessage.txt');
-      });
     res.status(200).send()
 })
 
--- runit.sh
+++ runit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
 docker build -f Dockerfile -t nodesqswatcher .
-docker tag nodesqswatcher:latest harbor.freshbrewed.science/freshbrewedprivate/nodesqswatcher:1.13
-docker push harbor.freshbrewed.science/freshbrewedprivate/nodesqswatcher:1.13
+docker tag nodesqswatcher:latest harbor.freshbrewed.science/freshbrewedprivate/nodesqswatcher:1.18
+docker push harbor.freshbrewed.science/freshbrewedprivate/nodesqswatcher:1.18
 
-sed -i 's/image:\(.*\):\([^:]*\)/image:\1:1.13/' sqswatcher.dep.yaml
+sed -i 's/image:\(.*\):\([^:]*\)/image:\1:1.18/' sqswatcher.dep.yaml
 kubectl apply -f sqswatcher.dep.yaml
